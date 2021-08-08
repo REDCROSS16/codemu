848ca7c7bc351e3aea15c80a31fbc8aa7368e60f
redcross16 <belkill@mail.ru>
2021-08-08 22:43:19 +0300

    codemu nav

--- drive/include/elems/nav/nav.php
+++ drive/include/elems/nav/nav.php
@@ -1,13 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="nav.css">
+</head>
+<body>
+    <ul class="root-nav">
+        <li><a href="#">1</a></li>
+        <li><span>Sub2</span>
+            <ul>
+                <li><a href="#">Main</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        </li>
+        <li><a href="#">3</a></li>
+        <li><span>Sub 4</span>
+            <ul>
+                <li><a href="#">100</a></li>
+                <li><span>200</span>
+                    <ul>
+                        <li><a href="#">Main</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contacts</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
 
-
-<ul class="root-nav">
-    <li><a href="#">1</a></li>
-    <li><a href="#">2</a>
-        <span>Sub2</span>
-        <ul>
-            
-        </ul>
-    </li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-</ul>
+                </li>
+                <li><a href="#">300</a></li>
+                <li><a href="#">400</a></li>
+            </ul>
+        </li>
+    </ul>
+    <script src="nav.js"></script>
+</body>
+</html>
--- drive/include/pages/about.php
+++ drive/include/pages/about.php
@@ -1,1 +1,5 @@
 <h2>About</h2>
+
+
+<?php
+include './elems/nav/nav.php';
