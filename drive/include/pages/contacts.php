8002d550d0d6dac9af5fdbe1dfa47a6a52a72713
redcross16 <belkill@mail.ru>
2021-08-13 00:10:56 +0300

    codemu nav

--- drive/include/pages/contacts.php
+++ drive/include/pages/contacts.php
@@ -1,1 +1,11 @@
-123
+<hr>
+<div>
+<p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at debitis delectus ea eligendi enim ipsa iusto minus modi necessitatibus optio pariatur quaerat qui quidem quos sunt tempora, ullam veritatis voluptas voluptatibus. Alias animi error eveniet illum, laborum minima sapiente ut. A accusamus accusantium aliquid aperiam, architecto asperiores commodi dignissimos dolores eius error exercitationem incidunt ipsum iure magni, maxime molestias, necessitatibus nemo neque officia praesentium qui reprehenderit sed sit sunt tempora tempore ullam unde velit voluptas voluptatem. Blanditiis, distinctio illo. Blanditiis consequatur cumque delectus dolorum error explicabo facilis laborum libero nemo, nisi numquam optio ratione repellat soluta temporibus tenetur voluptas.</p>
+<div>
+    <span>Alexander Podolnitskiy</span>
+</div>
+<div>
+    <span class="font">PHP Developer</span>
+</div>
+</div>
+
