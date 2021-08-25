84d1f9a08d085acb58890f023226c3b69e16bdf5
redcross16 <belkill@mail.ru>
2021-08-25 23:38:43 +0300

    codemu

--- drive/include/elems/header.php
+++ drive/include/elems/header.php
@@ -9,13 +9,17 @@ function createLink($href, $text) {
     }
     echo "<a href=\"$href\" $class>$text</a>";
 }
-createLink('/codemu/drive/include/', 'Main');
-createLink('/codemu/drive/include/?page=about', 'About');
-createLink('/codemu/drive/include/?page=contacts', 'Contacts');
+//createLink('/codemu/drive/include/', 'Main');
+//createLink('/codemu/drive/include/?page=about', 'About');
+//createLink('/codemu/drive/include/?page=contacts', 'Contacts');
 
 $query = 'SELECT * FROM pages';
 $result = mysqli_query($db, $query);
 
+for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
 
+foreach ( $data as $page) {
+    createLink('/codemu/drive/include/?page=' . $page['url'], $page['text']);
+}
 
 ?>
