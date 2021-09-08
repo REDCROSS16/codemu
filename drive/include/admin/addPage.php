27d8b248ac09fc1478659ee108f366104a8b7636
redcross16 <belkill@mail.ru>
2021-09-08 23:16:23 +0300

    codemu

---
+++ drive/include/admin/addPage.php
@@ -0,0 +1,11 @@
+<?php
+
+$out = '';
+$out .= '<form method="post" action="">'
+        . '<input name="pagename" class="">'
+        . '<textarea name="text" class=""></textarea>'
+    .'</form>';
+
+function addPage () {
+
+}
