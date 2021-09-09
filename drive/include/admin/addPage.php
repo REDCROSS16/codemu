bf8e1ca85faf56da6b989dc77d868af27f6c7af4
redcross16 <belkill@mail.ru>
2021-09-09 23:53:29 +0300

    codemu

--- drive/include/admin/addPage.php
+++ drive/include/admin/addPage.php
@@ -2,10 +2,17 @@
 
 $out = '';
 $out .= '<form method="post" action="">'
-        . '<input name="pagename" class="">'
+        . '<input name="title" class="">'
+        . '<input name="url" class="">'
         . '<textarea name="text" class=""></textarea>'
+    . '<input type="submit" class="">'
     .'</form>';
 
 function addPage () {
-
+    $title = $_POST['title'];
+    $url = $_POST['url'];
+    $text = $_POST['text'];
+    $db = ''; # !!!!
+    $query = 'INSERT INTO pages (title, url, text) VALUES('$title', '$url', '$text')';
+    mysqli_query($db, $query);
 }
