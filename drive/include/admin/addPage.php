199e582edb1d07419890ec99ad2c4682fee26d6e
redcross16 <belkill@mail.ru>
2021-09-16 22:07:12 +0300

    codemu

--- drive/include/admin/addPage.php
+++ drive/include/admin/addPage.php
@@ -2,9 +2,9 @@
 include '../db/db.php';
 
 $content = '<div style="display: flex; justify-content: center;align-items: center"><form method="post" action=""><br><br>'
-        . '<input name="title" class="form-control" style="width: 500px" placeholder="type title"><br><br>'
-        . '<input name="url" class="form-control" style="width: 500px" placeholder="type url"><br><br>'
-        . '<textarea name="text" class="form-control" style="width: 500px"></textarea><br><br>'
+        . '<input name="title" class="form-control" style="width: 500px" placeholder="type title" value="' . $_POST["title"] . '"><br><br>'
+        . '<input name="url" class="form-control" style="width: 500px" placeholder="type url" value="' . $_POST["url"] .'"><br><br>'
+        . '<textarea name="text" class="form-control" style="width: 500px">'. $_POST['text'] .'</textarea><br><br>'
     . '<input type="submit" name="submit" class="btn btn-primary">'
     .'</form></div>';
 
