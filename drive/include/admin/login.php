bc05ff25e29eb660fdd7b994dd894d8f8fff9ac8
redcross16 <belkill@mail.ru>
2021-11-11 23:56:16 +0300

    edit drive | флэш сообщения

--- drive/include/admin/login.php
+++ drive/include/admin/login.php
@@ -1,8 +1,10 @@
 <?php
 include '../elems/init.php';
 $title = 'login page';
+
+
 ?>
-<?php if (isset($_POST['password']) && $_POST['password'] == '123') {
+<?php if (isset($_POST['password']) && md5($_POST['password']) === '202cb962ac59075b964b07152d234b70') {
     $_SESSION['auth'] = true;
     $_SESSION['message'] = [
         'status' => 'success',
