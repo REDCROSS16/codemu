732b22caf8f8e14028c308ec1127e13f2406ce37
redcross16 <belkill@mail.ru>
2021-11-15 00:06:22 +0300

    edit drive | флэш сообщения

--- Practice/page1.php
+++ Practice/page1.php
@@ -2,9 +2,14 @@
 <p>Сделайте функцию, которая будет проверять пару чисел на дружественность. <br>Дружественные числа - два числа, для которых
     сумма всех собственных делителей первого числа равна второму числу и наоборот,<br>
     сумма всех собственных делителей второго числа равна первому числу. </p>
+<form action="#">
+    <input type="text" name="num1" placeholder="первое число">
+    <input type="text" name="num2" placeholder="второе число">
+    <input type="submit">
+</form>
 
 <?php
-
+# проверка на дружелюбность
 function checkOnFrendly($num1, $num2) {
     if (sumOfDivisors($num1) === $num2 && sumOfDivisors($num2) === $num1) {
         return true;
@@ -31,20 +36,17 @@ function sumOfDivisors($num) : int
     $result = getNumOfDivisors($num);
     return array_sum($result) - $num;
 }
-
-
-//var_dump(checkOnFrendly(220,284));
 ?>
 
 
-<form action="#">
-    <input type="text" name="num1" placeholder="первое число">
-    <input type="text" name="num2" placeholder="первое число">
-    <input type="submit">
-</form>
 <?php
     $num1 = $_GET['num1'];
     $num2 = $_GET['num2'];
 
-$result = checkOnFrendly($num1, $num2);
-echo "<h4></h4>h4>"
+$result = checkOnFrendly((int)$num1, (int)$num2);
+
+if ($result) {
+    echo '<h4 style="color:blue">Два числа дружелюбны!</h4>';
+} else {
+    echo '<h4 style="color:red">Два числа недружелюбны!</h4>';
+}
