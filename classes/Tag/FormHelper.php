1b75149ea92059dd33dc6a826b76dfbc3bbd26c5
redcross16 <belkill@mail.ru>
2021-11-27 23:15:35 +0300

    classes update

--- classes/Tag/FormHelper.php
+++ classes/Tag/FormHelper.php
@@ -46,13 +46,82 @@ class FormHelper extends TagHelper
         return $this->open('input', $attrs);
     }
 
+    // checkbox
     public function checkbox (array $attrs = []) : string
     {
         $attrs['type'] = 'checkbox';
         $attrs['value'] = 1;
 
-//        if (isset($attrs['name'])) {
-//            $name = $attrs['name'];
-//        }
+        if (isset($attrs['name'])) {
+            $name = $attrs['name'];
+            if (isset($_REQUEST[$name]) and $_REQUEST[$name] == 1) {
+                $attrs['checked'] = true;
+            }
+            $hidden = $this->hidden(['name' => $name, 'value' => 0]); 
+        } else {
+            $hidden = '';
+        }
+
+        return $hidden . $this->open('input', $attrs);
+    }
+
+    /**
+     * Метод для создания тега TEXTAREA
+     * */
+    public function textArea(array $attrs = [], string $text) : string
+    {
+        if (isset($attrs['name'])) {
+            $name = $attrs['name'];
+            if (isset($_REQUEST[$name])) {
+                $text = $_REQUEST[$name];
+            }
+        }
+        return $this->open('textarea', $attrs) . $text . $this->close('textarea');
+    }
+
+    /**
+     * Метод для создания тега SELECT
+     * */
+    public function select(array $selectAttrs = [], array $optionAttrs = []) : string
+    {
+        $options = '';
+
+        foreach ($optionAttrs as $optionAttr) {
+            $options .= $this->open('option', $optionAttr['attrs']) . $optionAttr['text'] . $this->close('option') ;
+        }
+
+        return $this->open('select', $selectAttrs) . $options . $this->close('select');
     }
 }
+
+
+
+$fh = new FormHelper();
+	
+	echo $fh->openForm(['action' => '', 'method' => 'GET']);
+	echo $fh->textArea(['name' => 'message'], 'abcdefg');
+	echo $fh->checkbox(['name' => 'c-1']);
+	echo $fh->submit(['name' => 'submit']);
+
+	echo $fh->closeForm();
+
+//
+$fh2 = new FormHelper();
+echo $fh2->openForm(['action' => '', 'method' => 'GET']);
+echo $fh2->select(
+    ['name' => 'list', 'class' => 'eee'],
+    [
+        ['text' => 'item1', 'attrs' => ['value' => '1']],
+        ['text' => 'item2', 'attrs' => ['value' => '1', 'selected' => true]],
+        ['text' => 'item1', 'attrs' => ['value' => '1', 'class' => 'last']],
+        ['text' => 'Витебск', 'attrs' => ['value' => 'Vitebsk', 'class' => 'last']],
+        ['text' => 'Витебск', 'attrs' => ['value' => 'Vitebsk', 'class' => 'last']],
+    ]
+);
+
+echo $fh2->select();
+
+var_dump($_REQUEST['list']);
+
+echo $fh2->submit();
+echo $fh2->closeForm();
